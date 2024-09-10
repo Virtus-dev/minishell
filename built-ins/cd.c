b9c0d72a501b859f07dc36d7c7c9a7d8fdac008e
virtus-dev <aritzgonzalez92@gmail.com>
2024-09-10 16:40:27 +0200

    changing come submodule fails

--- built-ins/cd.c
+++ built-ins/cd.c
@@ -1,35 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins.c                                         :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arigonza <arigonza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/22 10:03:14 by arigonza          #+#    #+#             */
-/*   Updated: 2024/08/23 12:16:17 by arigonza         ###   ########.fr       */
+/*   Created: 2024/09/09 15:09:50 by arigonza          #+#    #+#             */
+/*   Updated: 2024/09/10 16:39:52 by arigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../includes/minishell.h"
+#include "../libft/includes/libft.h"
 
-void echo(char* msg)
+void	ft_chdir(t_data *data, char *owd)
 {
-	printf("%s\n", msg);
+	char	*nwd;
+	
+	nwd = getcwd(nwd, NULL);
+
 }
 
-void cd(char* path)
+void	ft_cd(char* path)
 {
 	if (chdir(path) != 0) {
 		perror("cd");
 	}
 }
-
-void pwd()
+void	ft__pwd(t_data *data)
 {
-	char cwd[PATH_MAX];
-	if (getcwd(cwd, sizeof(cwd)) != NULL) {
-		printf("%s\n", cwd);
-	} else {
-		perror("pwd");
+	char cwd;
+
+    cwd = getcwd(NULL, 0);
+	if (cwd == NULL)
+		perror(PWD_ERR);
+    else
+    {
+		ft_putstr_fd(cwd, data->fdout);
+		ft_putchar_fd('\n', data->fdout);
 	}
+	free(cwd);
 }
--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: arigonza <arigonza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 10:03:31 by arigonza          #+#    #+#             */
-/*   Updated: 2024/08/23 12:16:53 by arigonza         ###   ########.fr       */
+/*   Updated: 2024/09/09 15:38:29 by arigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,32 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "constants.h"
+#include <sys/types.h>
+#include <sys/errno.h>
+#include <signal.h>
+
+// Structs
+typedef struct	s_token
+{
+    char	*str;
+    int		type;
+    int		space;
+    struct s_token	*prev;
+    struct s_token	*next;
+}	t_token;
+
+typedef struct	s_data
+{
+    char        *input;
+    char        **argv;
+    t_token		*token;
+    int         argc;
+    pid_t       child;
+    int         fdin;
+    int         fdout;
+} t_data;
+
+void    ft_echo(char *msg);
 
 
 #endif

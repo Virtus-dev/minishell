5b72e2739142e5ed3c500ae12e11ee8bb0181abc
Your Name <you@example.com>
2025-05-14 18:28:41 +0200

    parser

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 13:59:23 by fracurul          #+#    #+#             */
-/*   Updated: 2025/05/14 12:43:23 by fracurul         ###   ########.fr       */
+/*   Updated: 2025/05/14 15:58:05 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ int	check_line(char *line)
 		return (FALSE);
 	if (len == 1 && line[0] == '\n' && line[1] == '\0')
 		return (FALSE);
-	if (line[0] == '|' || line[0] == '>' || (line[0] == '<' && line[1] != '<')) // Mirar el heredoc "<< eof"
+	if (line[0] == '|' || line[0] == '>' || (line[0] == '<' && line[1] != '<'))
 		return (FALSE);
 	if (line[len - 1] == '|' || line[len - 1] == '>' || line[len - 1] == '<')
 		return (FALSE);
